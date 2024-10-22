d3e313ce573c54f1c64d65b2b00a8abd6548f342
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-22 11:23:48 +0530

    Improve clickable area of jquery tabs

--- without-plugin/js/script.js
+++ without-plugin/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$("#tabs ul li a").click(function() {
-		$(this).parent().siblings().removeClass("bg-success");
-		$(this).parent().addClass("bg-success");
-		const itemID = $(this).attr("href").slice(1);
-		$(this).parent().parent().nextAll("div").each(function() {
+	$("#tabs ul li").click(function() {
+		$(this).siblings().removeClass("bg-success");
+		$(this).addClass("bg-success");
+		const itemID = $(this).children().attr("href").slice(1);
+		$(this).parent().nextAll("div").each(function() {
 			if($(this).attr("id") == itemID) {
 				$(this).removeClass("d-none");
 			}
