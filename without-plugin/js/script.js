16beac41ff29ce94cecce040514e5bc13bd87a41
shreyastechv <shreyas.as@techversantinfotech.com>
2024-10-22 22:28:33 +0530

    22/10/2024 Update (#1)
    * Complete functionality of jquery-tabs without plugin
    
    * Improve clickable area of jquery tabs

--- without-plugin/js/script.js
+++ without-plugin/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	
+	$("#tabs ul li").click(function() {
+		$(this).siblings().removeClass("bg-success");
+		$(this).addClass("bg-success");
+		const itemID = $(this).children().attr("href").slice(1);
+		$(this).parent().nextAll("div").each(function() {
+			if($(this).attr("id") == itemID) {
+				$(this).removeClass("d-none");
+			}
+			else {
+				$(this).addClass("d-none");
+			}
+		})
+	});
 });
