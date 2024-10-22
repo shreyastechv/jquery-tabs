9b50914180f5fb3ebe9f572f19a4f5d1823f9346
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-22 11:21:38 +0530

    Complete functionality of jquery-tabs without plugin

--- without-plugin/js/script.js
+++ without-plugin/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	
+	$("#tabs ul li a").click(function() {
+		$(this).parent().siblings().removeClass("bg-success");
+		$(this).parent().addClass("bg-success");
+		const itemID = $(this).attr("href").slice(1);
+		$(this).parent().parent().nextAll("div").each(function() {
+			if($(this).attr("id") == itemID) {
+				$(this).removeClass("d-none");
+			}
+			else {
+				$(this).addClass("d-none");
+			}
+		})
+	});
 });
